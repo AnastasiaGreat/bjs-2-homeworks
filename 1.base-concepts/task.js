f45130f705366f00bfb97567047e98c4da435595
AnastasiaGreat <dart-war@mail.ru>
2022-09-04 16:27:05 +0400

    homework number 1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,19 @@
+'use strict'
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
+  let arr = [];
+  if (a === 0) {
+    arr[0] = -c / b;
+    return arr;
+  }
+  let discrim = b**2 - 4 * a * c;
+  if (discrim > 0) {
+    arr[0] = parseInt(( -b + Math.sqrt(discrim)) / (2 * a));
+    arr[1] = parseInt(( -b - Math.sqrt(discrim)) / (2 * a));
+  }
+  else if (discrim === 0) {
+    arr[0] = -b / (2 * a);
+  }
   return arr; // array
 }
 
