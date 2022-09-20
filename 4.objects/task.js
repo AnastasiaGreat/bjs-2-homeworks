416a29b5cd7c15c3943f14107153bd9b0f645328
AnastasiaGreat <dart-war@mail.ru>
2022-09-20 15:35:33 +0400

    new

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,38 @@
 function Student(name, gender, age) {
-    // Ваш код
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if(this.marks === undefined) {
+    this.marks = [mark];
+  } else {
+    this.marks.push(mark);
+  }
+}
+
+Student.prototype.addMarks = function (...marks) {
+  if (this.marks === undefined) {
+    this.marks = [...marks];
+  } else {
+    this.marks.push(...marks);
+  }
+}
+
+Student.prototype.getAverage = function (marks) {
+  let sum = this.marks.reduce ((acc, marks) => acc + marks, 0);
+  let length = this.marks.length;
+  return sum / length;
+}
+
+Student.prototype.exclude = function (reason) {
+  this.exclude = reason;
+  delete this.subject;
+  delete this.marks;
+}
